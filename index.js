df9a374535f1cb59a6c822856767b14ba5aad737
Eugene M <xexyz@yandex.ru>
2024-06-27 06:48:35 +0300

    ready on telegraf

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
+import express from "express";
 import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
+import { message, callbackQuery } from "telegraf/filters";
+import dotenv from "dotenv";
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+dotenv.config();
 
-bot.start((ctx) => ctx.reply("Добро пожаловать!"));
-bot.help((ctx) => ctx.reply("Пока не могу помочь, брат, давай сам"));
+const app = express();
 
-bot.command("send", (ctx) => {
-  return ctx.reply("Что хочешь отправить?");
-});
+const bot = new Telegraf(process.env.TELEGRAM_API_TOKEN);
+const PORT = process.env.PORT || 3000;
+
+bot.start((ctx) => ctx.reply("Привет, братишка"));
+bot.help((ctx) =>
+  ctx.reply(
+    "Сначала передай мне список каналов, через пробел.\n\nОчень важно, брат - в начале сообщения поставь 3 слэша - ///, чтобы я понимал, что это каналы.\n\nПотом просто напиши мне сообщение и я разошлю во все переданные тобой каналы.\n\nТакже не забудь, что я должен быть админом всех этих каналов"
+  )
+);
+
+let channels = [];
 
 bot.on(message(), (ctx) => {
-  return ctx.reply("Брат, пока не умею общаться");
+  if (ctx.message.text.startsWith("///")) {
+    channels = [...ctx.message.text.split(" ")];
+    channels.shift();
+    return;
+  }
+
+  if (!channels.length) {
+    ctx.reply(
+      "Извини, брат, некуда отправлять. Передай мне сначала список каналов"
+    );
+  } else {
+    channels.forEach((channel) => {
+      ctx.copyMessage(channel).catch(console.log);
+      ctx.reply(`Брат, отправил на ${channel}`);
+    });
+
+    channels = [];
+  }
+});
+
+bot.createWebhook({
+  domain: "https://moustru-statham-bot-d3d8.twc1.net",
 });
 
 bot.launch();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT} port`);
+});
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
